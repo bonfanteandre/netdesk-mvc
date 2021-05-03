6d954b29d42bd63c16ee400782670606a42db403
André Bonfante <andre@universovarejo.com.br>
2021-05-03 18:25:38 -0300

    Adicionando entidade de cliente

---
+++ src/NetDesk.Business/Entities/Client.cs
@@ -0,0 +1,16 @@
+﻿using NetDesk.Business.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDesk.Business.Entities
+{
+    public class Client : TEntity
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ src/NetDesk.Business/Entities/Common/TEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDesk.Business.Entities.Common
+{
+    public abstract class TEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ChangedOn { get; set; }
+    }
+}

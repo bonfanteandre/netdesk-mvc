38014bca398ca37a449fa3a35224df6f5e68d119
André Bonfante <andre@universovarejo.com.br>
2021-05-03 20:38:58 -0300

    Configurando AutoMapper

---
+++ src/NetDesk.App/Configurations/AutoMapperConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using NetDesk.App.Models;
+using NetDesk.Business.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetDesk.App.Configurations
+{
+    public class AutoMapperConfiguration : Profile
+    {
+        public AutoMapperConfiguration()
+        {
+            CreateMap<ClientViewModel, Client>()
+                .ReverseMap();
+        }
+    }
+}
---
+++ src/NetDesk.App/Models/ClientViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetDesk.App.Models
+{
+    public class ClientViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(13, MinimumLength = 8)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 9)]
+        public string Email { get; set; }
+    }
+}
